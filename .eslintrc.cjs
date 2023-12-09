64c5ea995f6c6f4485c5bfd9900d748d772f136c
Misha Ganin <gma-13@yandex.ru>
2023-12-09 09:42:15 +0300

    feat(lint): resolve eslint and prettier conflicts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'airbnb',
     'airbnb-typescript',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
