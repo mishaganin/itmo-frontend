4b62b9fedf6249a53dd3f4a02b382d1243b19426
Misha Ganin <gma-13@yandex.ru>
2024-05-25 20:16:02 +0300

    fix(ts): resolve ts errors

--- src/shared/utils/getCookieByName.ts
+++ src/shared/utils/getCookieByName.ts
@@ -1,7 +1,7 @@
 export function getCookieByName(cookies: string, name: string) {
   const value = `; ${cookies}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+  if (parts.length === 2) return parts.pop()?.split(';').shift();
 
   return '';
 }
